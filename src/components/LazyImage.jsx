a9c1ef82ea56c7f3f2d1d5bd08c9563cf0df8361
Vanessa Kang <vk@Vanessas-MBP.broadband>
2020-12-03 16:20:13 +0000

    update media query max-width 415px, to apply only to gifts background-img

--- src/components/LazyImage.jsx
+++ src/components/LazyImage.jsx
@@ -9,7 +9,7 @@ const [ref, inView] = useInView({
   rootMargin: '0px 0px -50px 0px'
 });
 
-return  <div ref={ref} className='background-img-wrapper'>
+return  <div ref={ref} className='background-img-wrapper' id={props.id}>
   {inView && <img className='background-img background-img-0'  src={props.imgURL0} alt='background-img' /> }
  {(inView && props.imgURL1)? <img className='background-img background-img-1'   src={props.imgURL1} alt='background-img' />: null}
  {(inView && props.imgURL2) ?<img className='background-img background-img-2'   src={props.imgURL2} alt='background-img' /> : null} 
