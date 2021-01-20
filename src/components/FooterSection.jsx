14ba3f73245c54431c81247fc4ff72a100f6c430
Vanessa Kang <vk@Vanessas-MBP.broadband>
2021-01-20 15:38:14 +0000

    update meta tag

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import AdditionalInformation from './Footer/AdditionalInformation.jsx';
-import Directory from './Footer/Directory.jsx';
+import React from 'react'
+import AdditionalInformation from './Footer/AdditionalInformation.jsx'
+import Directory from './Footer/Directory.jsx'
 import FooterNav from './Footer/FooterNav.jsx'
 
 const Footer = () => {
-
-  return <section className='footer-section'><div className='footer-container'>
-  <AdditionalInformation />
-    <Directory />
-    <FooterNav />
-  </div></section>
+    return (
+        <section className='footer-section'>
+            <div className='footer-container'>
+                <AdditionalInformation />
+                <Directory />
+                <FooterNav />
+            </div>
+        </section>
+    )
 }
 
-export default Footer;
+export default Footer
